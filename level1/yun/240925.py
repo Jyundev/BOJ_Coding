503b0b7c8444a71a3f09b6fb4a51fe904e0e7685
Jyundev <jyunxxxxx@gmail.com>
2024-09-27 20:44:21 +0900

    240927

--- level1/yun/240925.py
+++ level1/yun/240925.py
@@ -62,3 +62,73 @@ def solution(name, yearning, photo):
     return answer
 
 # 카드 뭉치
+def solution(cards1, cards2, goal):
+    idx1 = -1
+    idx2 = -1
+    for g in goal:
+        if g in cards1:
+            if idx1 > cards1.index(g):
+                return "No"
+            else:
+                idx1 = cards1.index(g) 
+                cards1.remove(g)
+                if idx1==len(cards1) and len(cards1)>0:
+                    return "No"
+                
+        else:
+            if idx2 > cards2.index(g):
+                return "No"
+            else: 
+                idx2 = cards2.index(g)   
+                cards2.remove(g)
+                if idx2==len(cards2) and len(cards2)>0:
+                    return "No"
+            
+    return "Yes"
+
+
+# 다른 풀이 
+def solution(cards1, cards2, goal):
+    for g in goal:
+        if len(cards1) > 0 and g == cards1[0]:
+            cards1.pop(0)       
+        elif len(cards2) >0 and g == cards2[0]:
+            cards2.pop(0)
+        else:
+            return "No"
+    return "Yes"
+
+# 폰켓몬
+def solution(nums):
+    type = set(nums)
+    if len(type)>=len(nums) // 2:
+        return len(nums) // 2
+    else:
+        return len(type)
+
+
+
+def solution(ls):
+    return min(len(ls)/2, len(set(ls)))
+
+
+# 2016년
+
+def solution(a, b):
+    weeks = ["SUN","MON","TUE","WED","THU","FRI","SAT"]
+    days = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]    
+    answer = weeks[(sum(days[:a-1])+b+4)%7]
+    
+    return answer
+
+# 다른 풀이 
+
+import datetime
+
+def getDayName(a,b):
+    t = 'MON TUE WED THU FRI SAT SUN'.split()
+    return t[datetime.datetime(2016, a, b).weekday()]
+
+
+# 기사단원의 무기
+
